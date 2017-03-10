1e3a1b85b716948628ec9ac9422c42d37383619d
Andrei <andrei.tichenkov@gmail.com>
2017-03-10 15:36:47 -0500

    Add prof code endpoint

--- attendance/attendance/prof/views.py
+++ attendance/attendance/prof/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import random
+import json
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    if request.method == 'GET':
+        code = random.randrange(0,9999)
+        codestr = str(code).zfill(4)
+        returndict = {"code" : codestr}
+        return HttpResponse(json.dumps(returndict), content_type="application/json")
+
 
 # Create your views here.
