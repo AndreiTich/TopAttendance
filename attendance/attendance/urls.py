b3d89bc50113ce319b73cca898f36952cbc7d386
Surag Sudesh <suragsudesh@hotmail.com>
2017-03-10 15:18:42 -0500

    Remove extra admin url

--- attendance/attendance/urls.py
+++ attendance/attendance/urls.py
@@ -18,7 +18,6 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^prof/',include('attendance.prof.urls')),
-    url(r'^admin/', admin.site.urls),
     url(r'^student/', include('attendance.student.urls')),
     url(r'^admin/', admin.site.urls),
 ]
