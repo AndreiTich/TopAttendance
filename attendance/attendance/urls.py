678252cdbec863cba65f924813b824818fb0a9de
Surag Sudesh <suragsudesh@hotmail.com>
2017-03-10 14:53:02 -0500

    Moved prof and student apps

--- attendance/attendance/urls.py
+++ attendance/attendance/urls.py
@@ -14,8 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
 from django.conf.urls import url
+from django.conf.urls import include, url
 from django.contrib import admin
 
 urlpatterns = [
+    url(r'^admin/', admin.site.urls),
+    url(r'^student/', include('attendance.student.urls')),
     url(r'^admin/', admin.site.urls),
 ]
