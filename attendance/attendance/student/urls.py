09e08edd83d8673e04969cf93f5a9b2dd48ba915
ling meng <menglingsilence@gmail.com>
2017-03-10 15:50:43 -0500

    student endpoint

--- attendance/attendance/student/urls.py
+++ attendance/attendance/student/urls.py
@@ -1,7 +1,6 @@
 from django.conf.urls import url
-
 from . import views
 
 urlpatterns = [
-    url(r'attendance-code/$', views.index, name='index'),
+    url(r'attendance-code/$', views.attendance_code),
 ]
--- attendance/attendance/student/views.py
+++ attendance/attendance/student/views.py
@@ -1,7 +1,20 @@
+import json
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseBadRequest
+from django.views.decorators.http import require_POST
+from django.views.decorators.csrf import csrf_exempt
 
 
-def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+@require_POST
+@csrf_exempt
+def attendance_code(request):
+    json_data = json.loads(request.body)
+    try:
+        student_id = json_data['student_id']
+        code = json_data['code']
+    except KeyError:
+        return HttpResponseBadRequest()
+
+    return HttpResponse("student_id: {}, code: {}".format(student_id, code))
+
 # Create your views here.
