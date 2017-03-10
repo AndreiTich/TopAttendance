b2ba29576bfac72abb5230d1198c3ca845a95ab6
Alex Ehlke <alex.ehlke@gmail.com>
2017-03-10 15:09:38 -0500

    fix urls

--- attendance/attendance/urls.py
+++ attendance/attendance/urls.py
@@ -17,8 +17,7 @@ from django.conf.urls import include, url
 from django.contrib import admin
 
 urlpatterns = [
-    url(r'^student/',include('student.urls')),
-    url(r'^prof/',include('prof.urls')),
+    url(r'^prof/',include('attendance.prof.urls')),
     url(r'^admin/', admin.site.urls),
     url(r'^student/', include('attendance.student.urls')),
     url(r'^admin/', admin.site.urls),
