dbf1f593381e748bbbae6e01d35d74f272802328
Alex Ehlke <alex.ehlke@gmail.com>
2017-03-11 11:59:17 -0500

    add bpython

--- attendance/attendance/settings.py
+++ attendance/attendance/settings.py
@@ -27,6 +27,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['top-attendance.herokuapp.com']
 
+SHELL_PLUS = 'bpython'
 
 # Application definition
 
@@ -39,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'attendance.prof',
     'attendance.student',
+    'django_extensions',
 ]
 
 MIDDLEWARE = [
