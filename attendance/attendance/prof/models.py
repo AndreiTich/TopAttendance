7b625d1cbd5084125c23a5164516b6d1a18e696f
Hannan Zubair <hannanthetron@v1020-wn-113-29.campus-dynamic.uwaterloo.ca>
2017-03-10 19:39:59 -0500

    basic default data model for prof

--- attendance/attendance/prof/models.py
+++ attendance/attendance/prof/models.py
@@ -2,3 +2,5 @@ from django.db import models
 
 class Attendance(models.Model):
     class_code = models.CharField(max_length=4)
+    latitude = models.DecimalField(max_digits=9, decimal_places=6, default=0)
+    longitude = models.DecimalField(max_digits=9, decimal_places=6, default=0)
