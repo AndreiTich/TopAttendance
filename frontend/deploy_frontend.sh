37823347cbdea5efe4148dd1eeb74fc1ee11a9f2
Daniel Gipps <daniel.gipps@tophat.com>
2017-03-11 14:16:30 -0500

    Small change to deploy_frontend

--- frontend/deploy_frontend.sh
+++ frontend/deploy_frontend.sh
@@ -1,6 +1,6 @@
 npm install
-npm run build
 rm -r ../attendance/static/
+mkdir ../attendance/static
+npm run build
 cp build/index.html ../attendance/attendance/templates/index.html
 cp -r build/static/* ../attendance/static/
-
