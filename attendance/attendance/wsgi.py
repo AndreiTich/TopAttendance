19120c797f6c01895e82b66f14510f0d7f0df9bd
Alex Ehlke <alex.ehlke@gmail.com>
2017-03-11 10:09:08 -0500

    try to fix paths

--- attendance/attendance/wsgi.py
+++ attendance/attendance/wsgi.py
@@ -7,9 +7,11 @@ For more information on this file, see
 https://docs.djangoproject.com/en/1.10/howto/deployment/wsgi/
 """
 
-import os
+import os, sys
 
 from django.core.wsgi import get_wsgi_application
+
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "attendance.attendance.settings")
 
